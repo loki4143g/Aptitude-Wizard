512f9be8be7a1eca6ec9985e94f3bd7741d910b9
loki4143g <94797500+loki4143g@users.noreply.github.com>
2025-02-02 07:32:04 +0000

    database added

--- awp/settings.py
+++ awp/settings.py
@@ -80,11 +80,7 @@ WSGI_APPLICATION = 'awp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        dj_database_url.config(default=os.getenv('DATABASE_URL'))
-    }
-}
+DATABASES["default"] = dj_database_url.parse(os.getenv('DATABASE_URL'))
 
 
 # Password validation
