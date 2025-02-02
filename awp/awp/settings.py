653321e56de432e66055bdb92a2a040b9797323f
loki4143g <94797500+loki4143g@users.noreply.github.com>
2025-02-02 06:25:30 +0000

    second commit

--- awp/awp/settings.py
+++ awp/awp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,8 +82,7 @@ WSGI_APPLICATION = 'awp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        dj_database_url.config(default=os.getenv('DATABASE_URL'))
     }
 }
 
